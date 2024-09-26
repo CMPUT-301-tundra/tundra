5912082c0a0b1bc252ce250a4bb6ce469c373a45
kifidon <113075637+kifidon@users.noreply.github.com>
2024-09-26 15:45:53 -0600

    Creating merge conflict

--- app/src/main/java/com/example/tundra/Shape.java
+++ app/src/main/java/com/example/tundra/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
 
-    String color = "red";
+    String color = "Green";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
