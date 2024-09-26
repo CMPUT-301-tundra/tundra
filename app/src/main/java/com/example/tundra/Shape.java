371e71708d47e1b9d1e66cdbe2aa1c7adf7c6dd3
kifidon <113075637+kifidon@users.noreply.github.com>
2024-09-26 15:19:07 -0600

    Adding colour field

--- app/src/main/java/com/example/tundra/Shape.java
+++ app/src/main/java/com/example/tundra/Shape.java
@@ -3,7 +3,7 @@ package com.example.tundra;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-
+    private String colour = "Blue";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
