c9f90c193e554128b74953e682ee4718ab5801c9
iudex333 <matthewgao20@gmail.com>
2024-09-26 16:41:49 -0600

    updated colour

--- app/src/main/java/com/example/tundra/Shape.java
+++ app/src/main/java/com/example/tundra/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
 
-    String color = "Purple";
+    String color = "magenta";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
