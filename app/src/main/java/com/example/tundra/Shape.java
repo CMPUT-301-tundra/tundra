520428291a84b642a072f73b8a009496addc51bd
ryanalmasii <ralmasi@48hourdiscovery.com>
2024-09-26 15:47:49 -0600

    Color

--- app/src/main/java/com/example/tundra/Shape.java
+++ app/src/main/java/com/example/tundra/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
 
-    String color = "Yellow";
+    String color = "Blue";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
